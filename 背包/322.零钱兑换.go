bef5527f8213eb1bb48abffe8e697a7771946a90
5p4de7 <jwj12712@gmail.com>
2025-06-16 11:16:08 +0800

    publish try

--- 背包/322.零钱兑换.go
+++ 背包/322.零钱兑换.go
@@ -7,7 +7,8 @@ import "math"
 // 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
 //
 // 你可以认为每种硬币的数量是无限的。
-func coinChange2(coins []int, amount int) (ans int) {
+
+func CoinChange2(coins []int, amount int) (ans int) {
 	n := len(coins)
 	memo := make([][]int, n)
 	for i := range memo {
@@ -46,3 +47,4 @@ func coinChange2(coins []int, amount int) (ans int) {
 //* dfs
 //* 动态规划
 //todo 加入贪婪算法
+//shiyan
