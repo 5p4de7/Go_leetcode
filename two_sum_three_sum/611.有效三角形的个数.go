c88654dac50a0c361762b1d2d56bd863182f79bc
5p4de7 <jwj12712@gmail.com>
2025-06-17 19:46:27 +0800

    结束三数和

--- two_sum_three_sum/611.有效三角形的个数.go
+++ two_sum_three_sum/611.有效三角形的个数.go
@@ -1,61 +1,44 @@
 package two_sum_three_sum
 
 import (
-	"fmt"
 	"sort"
 )
 
 //* 给定一个包含非负整数的数组 nums ，返回其中可以组成三角形三条边的三元组个数。
 
-func cN2Cal(left, right int) int {
-	n := right - left + 1
-
-	ans := int(0.5 * float64(n) * float64(n-1))
-	return ans
-}
 func TriangleNumber(nums []int) int {
 	sort.Ints(nums)
 	ans := 0
 	n := len(nums)
 	for a := 0; a < n-2; a++ {
-		b := a + 1
-		c := n - 1
-		left := b
-		right := c
-		cur1, cur2 := nums[a]+nums[b], nums[a]+nums[b]
-		if cur1 > nums[c] {
-			ans = ans + cN2Cal(b, c)
-			fmt.Printf("ans is %v\n", b)
-			fmt.Printf("结束\n")
-			continue
-
+		for b := a + 1; b < n-1; b++ {
+			c := n - 1
+			for b < c {
+				if nums[a]+nums[b] <= nums[c] {
+					c--
+				} else {
+					ans = ans + c - b
+					break
+				}
+			}
 		}
-		for left < c && cur1 <= nums[c] {
-			left++
-			//println(left)
-			//println("结束")
-			cur1 = nums[a] + nums[left]
+	}
+	return ans
+}
+func Ans_triangleNumber(nums []int) int {
+	sort.Ints(nums)
+	ans := 0
+	n := len(nums)
+	for c := 2; c < n; c++ {
+		a, b := 0, c-1
+		for a < b {
+			if nums[a]+nums[b] > nums[c] {
+				ans = ans + b - a
+				b--
+			} else {
+				a++
+			}
 		}
-		for b < right && cur2 <= nums[right] {
-			right--
-			//println(right)
-			cur2 = nums[a] + nums[b]
-		}
-		if left == c || right == b {
-			continue
-		}
-		fmt.Printf("left: %d,right: %d\n", left, right)
-		fmt.Printf("b:%d,c:%d\n", b, c)
-		var bonus int
-		if left < right {
-			bonus = cN2Cal(left, c) + cN2Cal(b, right) - cN2Cal(left, right)
-		} else {
-			bonus = cN2Cal(left, c) + cN2Cal(b, right) + cN2Cal(right, left)
-		}
-
-		fmt.Printf("bonus: %d\n", bonus)
-		ans = ans + bonus
-		fmt.Printf("ans is %v\n", b)
 	}
 	return ans
 }
